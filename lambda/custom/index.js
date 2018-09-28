5a6a47f4447c2147fd19b4a51e6ec72429d7d19a
Jake Kelly <jakemkelly84@gmail.com>
2018-09-28 13:43:57 -0600

    Add file structure and localization interceptor

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -26,6 +26,12 @@
 /* eslint-disable  no-console */
 
 const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+
+const languageStrings = {
+  'en': require('./languages/en.js'),
+};
 
 /* INTENT HANDLERS */
 
@@ -45,7 +51,7 @@ const CouchPotatoIntent = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
 
-    return request.type === 'IntentRequest' 
+    return request.type === 'IntentRequest'
       && request.intent.name === 'CouchPotatoIntent';
   },
   handle(handlerInput) {
@@ -143,7 +149,7 @@ const HelpHandler = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
 
-    return request.type === 'IntentRequest' 
+    return request.type === 'IntentRequest'
       && request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
@@ -299,7 +305,37 @@ function getSlotValues(filledSlots) {
   }, this);
 
   return slotValues;
-}
+};
+
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    const localizationClient = i18n.use(sprintf).init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      resources: languageStrings,
+    });
+    localizationClient.localize = function localize() {
+      const args = arguments;
+      const values = [];
+      for (let i = 1; i < args.length; i += 1) {
+        values.push(args[i]);
+      }
+      const value = i18n.t(args[0], {
+        returnObjects: true,
+        postProcess: 'sprintf',
+        sprintf: values,
+      });
+      if (Array.isArray(value)) {
+        return value[Math.floor(Math.random() * value.length)];
+      }
+      return value;
+    };
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function translate(...args) {
+      return localizationClient.localize(...args);
+    };
+  },
+};
+
 
 exports.handler = skillBuilder
   .addRequestHandlers(
@@ -309,7 +345,8 @@ exports.handler = skillBuilder
     CompletedRecommendationIntent,
     HelpHandler,
     ExitHandler,
-    SessionEndedRequestHandler,    
+    SessionEndedRequestHandler,
   )
+  .addRequestInterceptors(LocalizationInterceptor)
   .addErrorHandlers(ErrorHandler)
   .lambda();
